852082b990816ffbd98db24c237056af27007e6b
manojprogramer <kasumanoj81@gmail.com>
2026-09-25 18:19:28 +0530

    25-09-2026

--- services/job-portal-resume-service/src/main/java/com/manoj/job/job_portal_resume_service/service/impl/WorkExperienceServiceImpl.java
+++ services/job-portal-resume-service/src/main/java/com/manoj/job/job_portal_resume_service/service/impl/WorkExperienceServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service0
+@Service
 public class WorkExperienceServiceImpl implements WorkExperienceService {
     @Override
     public WorkExperienceResponse addWorkExperience(Long resumeId, Long candidateId, AddWorkExperienceRequest request) {
